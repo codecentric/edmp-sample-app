4bc567b43e1b1a9d0cbfafa334b81287dec87d66
tobiasflohre <tobias.flohre@codecentric.de>
2016-02-22 22:55:01 +0100

    Initial sample app

---
+++ src/main/java/de/codecentric/EdmpSampleAppApplication.java
@@ -0,0 +1,12 @@
+package de.codecentric;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdmpSampleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdmpSampleAppApplication.class, args);
+	}
+}
